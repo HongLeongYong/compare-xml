77bd81c329299e34fb3918891ada973f85bc74f3
hongleongyong <yonghongleong98@gmail.com>
2023-08-24 14:56:11 +0800

    get different file.py v5

--- 03 get different file.py
+++ 03 get different file.py
@@ -2,8 +2,17 @@ import os
 import time
 import global_variable as gv
 
+
+def move_file(file_name, source_directory, target_directory):
+    source_file = os.path.join(source_directory, file_name)
+    target_file = os.path.join(target_directory, file_name)
+    os.rename(source_file, target_file)
+
+
 time_start = time.time()
 print("開始執行")
+
+move_file_boolean = False
 
 # 判斷 after directory 有，before directory 沒有的檔案
 before_file_names = os.listdir(gv.before_file_directory)
@@ -37,6 +46,14 @@ with open(os.path.join(gv.result_directory, "result.txt"), "w", encoding="utf-8"
         f.write("after directory 有, before directory 沒有的檔案: "+ str(len(after_file_names)) + "\n")
         for file in after_file_names:
             f.write(file + "\n")
+
+if move_file_boolean:
+    for file in before_file_names_2:
+        move_file(file, gv.before_file_directory, gv.result_directory)
+
+    for file in after_file_names:
+        move_file(file, gv.after_file_directory, gv.result_directory)
+
 
 time_end = time.time()
 print("花費時間: ", time_end - time_start, "秒")
