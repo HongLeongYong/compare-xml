40dbf2792e49842d4ef6e2c81f9e7992412a72fc
hongleongyong <yonghongleong98@gmail.com>
2023-08-25 08:51:36 +0800

    update readme.md

--- global_variable.py
+++ global_variable.py
@@ -1,7 +1,10 @@
+# 大 xml 檔案路徑
 before_big_file_directory = r"C:\Users\user\Desktop\test\before"
 after_big_file_directory = r"C:\Users\user\Desktop\test\after"
 
+# 分割後的 xml 檔案路徑
 before_file_directory = r"C:\Users\user\Desktop\test\before\before"
 after_file_directory = r"C:\Users\user\Desktop\test\after\after"
 
+# 結果檔案路徑
 result_directory = r"C:\Users\user\Desktop\test\result"
