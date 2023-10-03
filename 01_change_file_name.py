a4168a00305c609f613c5da500ad73a1af0ece64
hongleongyong <yonghongleong98@gmail.com>
2023-10-03 11:32:46 +0800

    update code 01 change file name

--- 01_change_file_name.py
+++ 01_change_file_name.py
@@ -1,19 +1,17 @@
-#  loop 資料夾底下的檔案，並且更改檔案名稱
+"""Module for renaming files in a directory and appending a file extension."""
 import os
 import global_variable as gv
 
 def run(directory):
-    # 設定副檔名
+    """Rename files in the given directory and append a file extension.
+    
+    Args:
+        directory (str): The directory containing the files to be renamed.
+    """
     file_type = ".xml"
-
     for file in os.listdir(directory):
         os.rename(os.path.join(directory, file), os.path.join(directory, file + file_type))
-        
-    # 顯示 for 的總數量
     print("Total: " + str(len(os.listdir(directory))))
 
-# -------------------------------------------------------------------------------
 run(gv.before_big_file_directory)
 run(gv.after_big_file_directory)
-
-
