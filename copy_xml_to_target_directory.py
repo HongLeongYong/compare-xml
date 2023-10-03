bff0f55f99ca5b5deb4d2f5ce9601ab922212eb1
hongleongyong <yonghongleong98@gmail.com>
2023-10-03 14:58:50 +0800

    可成長性架構

--- copy_xml_to_target_directory.py
+++ copy_xml_to_target_directory.py
@@ -1,18 +1,31 @@
-# copy xml file to target directory
+'''
+複製檔案到目標資料夾並重新命名
+'''
 import os
-import global_variable as gv
 import shutil
+import global_variable as gv
 
-file_list = ['abc.xml', 'def.xml', 'ghi.xml']
+def copy_and_rename_files(file_list):
+    '''
+    loop through file_list
+    '''
+    for file_name in file_list:
+        # Copy 'before' file to target directory and rename it
+        copy_and_rename(file_name, gv.before_file_directory, '_before.xml')
 
+        # Copy 'after' file to target directory and rename it
+        copy_and_rename(file_name, gv.after_file_directory, '_after.xml')
 
-for file in file_list:
-    # copy before file to target directory
-    shutil.copy(os.path.join(gv.before_file_directory, file), gv.result_directory)
-    # rename file
-    os.rename(os.path.join(gv.result_directory, file), os.path.join(gv.result_directory, file.replace('.xml', '_before.xml')))
+def copy_and_rename(file_name, source_directory, suffix):
+    '''
+    Copy file from source directory to target directory and rename it
+    '''
+    shutil.copy(os.path.join(source_directory, file_name), gv.result_directory)
+    os.rename(
+        os.path.join(gv.result_directory, file_name),
+        os.path.join(gv.result_directory, file_name.replace('.xml', suffix))
+    )
 
-    # copy after file to target directory
-    shutil.copy(os.path.join(gv.after_file_directory, file), gv.result_directory)
-    # rename file
-    os.rename(os.path.join(gv.result_directory, file), os.path.join(gv.result_directory, file.replace('.xml', '_after.xml')))
+if __name__ == "__main__":
+    input_file_list = ['abc.xml', 'def.xml', 'ghi.xml']
+    copy_and_rename_files(input_file_list)
