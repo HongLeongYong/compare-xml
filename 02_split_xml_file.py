9a7850d4416f825f626e08910ea317c54cea11b5
hongleongyong <yonghongleong98@gmail.com>
2023-10-04 09:52:55 +0800

    update split xml file.py

--- 02_split_xml_file.py
+++ 02_split_xml_file.py
@@ -1,62 +1,67 @@
 """
-此模組用於分割 XML 檔案。
+此模組用於讀取 XML 檔案，將其分割成多個段落，並儲存為新的 XML 檔案。
 """
 
 import os
 import time
 import xml.etree.ElementTree as ET
-import global_variable as gv  # 自定義模組放在標準模組之後
+import global_variable as gv
 
-# 常數名稱改為大寫
-DESTINATION = gv.before_file_directory
-
-def split_xml_file(filename, destination_directory):
+def split_and_save_xml_segments(filename, destination_directory):
     """
-    此函數用於分割 XML 檔案。
+    讀取 XML 檔案，將其分割成多個段落，並儲存為新的 XML 檔案。
     """
-    # 使用 'with' 語句來自動關閉檔案
     with open(filename, "r", encoding="utf-8") as xml_file:
         xml_string = xml_file.read()
 
-    # 取代掉不需要的標籤
     xml_string = xml_string.replace("<sf>", "").replace("</sf>", "")
-
-    # 加入特殊標籤做分割
     xml_string = xml_string.replace("</asx:abap>", "</asx:abap>@@@qq###")
-    xml_obj_array = xml_string.split("@@@qq###")
-    xml_obj_array.pop()
+    xml_segments = xml_string.split("@@@qq###")
+    xml_segments.pop()
 
-    for xml_obj in xml_obj_array:
-        tree = ET.ElementTree(ET.fromstring(xml_obj))
+    for segment_index, xml_segment in enumerate(xml_segments):
+        tree = ET.ElementTree(ET.fromstring(xml_segment))
         root = tree.getroot()
 
-        # 檔案名稱
-        cc_num = root.find('.//CC_NUM').text
-        template_id = root.find('.//TEMPLATE_ID').text
+        doc_temp_id = root.find('.//DOC_TEMP_ID').text
+        bp_id = root.find('.//BUSINESS_PARTNER_ID').text
+        commission_contract = root.find('.//COMMISSION_CONTRACT').text
+        policy_id = root.find('.//POLICY_ID').text
 
-        # xml_declaration 表示是否要加入 xml 宣告
-        tree.write(os.path.join(destination_directory, f"{cc_num}_{template_id}.xml"),
+        output_name = f"{doc_temp_id}_{bp_id}_{commission_contract}_{policy_id}_{segment_index}.xml"
+        output_name = output_name.replace('/', '_')
+
+        tree.write(os.path.join(destination_directory, output_name),
                    encoding="utf-8", xml_declaration=True)
 
-def main():
-    """
-    主函數。
-    """
+def process_files_in_directory(from_path, destination_path):
+    '''
+    遍歷指定目錄下的所有 XML 檔案，將其分割並儲存到目標目錄。
+    '''
     time_start = time.time()
     print(f"Start time: {time_start}")
 
-    if not os.path.exists(DESTINATION):
-        os.makedirs(DESTINATION)
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
 
-    for index, file in enumerate(os.listdir(gv.before_big_file_directory)):
-        split_xml_file(filename=os.path.join(gv.before_big_file_directory, file),
-                       destination_directory=DESTINATION)
+    sorted_file_list = sorted(os.listdir(from_path))
+
+    for index, file in enumerate(sorted_file_list):
+        split_and_save_xml_segments(filename=os.path.join(from_path, file),
+                                    destination_directory=destination_path)
         if index % 100 == 0:
             print(f"Processing {index + 1} files")
 
     time_end = time.time()
     print(f"End time: {time_end}")
     print(f"Total time: {time_end - time_start}")
+
+def main():
+    """
+    主函數，負責調用其他函數以完成 XML 檔案的分割和儲存。
+    """
+    process_files_in_directory(gv.before_big_file_directory, gv.before_file_directory)
+    process_files_in_directory(gv.after_big_file_directory, gv.after_file_directory)
 
 if __name__ == "__main__":
     main()
